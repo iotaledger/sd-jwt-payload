1478d55b83877e56a7fbaa23a2882847bf8ef66e
Abdulrahim Al Methiab <31316147+abdulmth@users.noreply.github.com>
2023-11-30 18:48:17 +0100

    Format fix (#2)

--- src/disclosure.rs
+++ src/disclosure.rs
@@ -1,9 +1,9 @@
 // Copyright 2020-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use std::fmt::Display;
 use crate::Error;
 use serde_json::Value;
+use std::fmt::Display;
 
 /// Represents an elements constructing a disclosure.
 /// Object properties and array elements disclosures are supported.
