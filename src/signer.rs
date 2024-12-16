510ac953d3e41560060f626757fc4ee870656b82
umr1352 <enrico.marconi@iota.org>
2024-12-16 17:06:25 +0100

    signer's error is display

--- src/signer.rs
+++ src/signer.rs
@@ -1,7 +1,7 @@
 // Copyright 2020-2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use std::error::Error;
+use std::fmt::Display;
 
 use async_trait::async_trait;
 use serde_json::Map;
@@ -12,7 +12,7 @@ pub type JsonObject = Map<String, Value>;
 /// JSON Web Signature (JWS) Signer.
 #[async_trait]
 pub trait JwsSigner {
-  type Error: Error;
+  type Error: Display;
   /// Creates a JWS. The algorithm used for signed must be read from `header.alg` property.
   async fn sign(&self, header: &JsonObject, payload: &JsonObject) -> Result<Vec<u8>, Self::Error>;
 }
